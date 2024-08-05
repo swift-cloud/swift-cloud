14ebf9bf8f931fa8747473111a68089f57928b11
Andrew Barba <barba@hey.com>
2024-08-05 12:03:25 -0400

    Cleanup

--- Sources/Cloud/AWS/Function.swift
+++ Sources/Cloud/AWS/Function.swift
@@ -25,20 +25,19 @@ extension aws {
                 properties: [
                     "assumeRolePolicy": Resource.JSON(
                         """
-                            {
-                                "Version": "2012-10-17",
-                                "Statement": [
-                                    {
-                                        "Effect": "Allow",
-                                        "Principal": {
-                                            "Service": "lambda.amazonaws.com"
-                                        },
-                                        "Action": "sts:AssumeRole"
-                                    }
-                                ]
-                            }
-                        """
-                    )
+                        {
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Principal": {
+                                        "Service": "lambda.amazonaws.com"
+                                    },
+                                    "Action": "sts:AssumeRole"
+                                }
+                            ]
+                        }
+                        """)
                 ]
             )
             rolePolicyAttachment = Resource(
