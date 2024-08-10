6353b74dc1a4d4f330a6717f7097399f556b4447
Andrew Barba <barba@hey.com>
2024-08-10 15:09:08 -0400

    Revert

--- Sources/Cloud/Docker/Docker.swift
+++ Sources/Cloud/Docker/Docker.swift
@@ -23,15 +23,11 @@ extension Docker.Dockerfile {
 
     public static func amazonLinux(targetName: String, architecture: Architecture = .current, port: Int) -> String {
         """
-        FROM amazonlinux:2023 as context
+        FROM amazonlinux:2023
 
         WORKDIR /app/
 
         COPY ./.build/\(architecture.swiftBuildLinuxDirectory)/release/\(targetName) .
-
-        FROM amazonlinux:2023
-
-        COPY --from=context /app/\(targetName) .
 
         ENV SWIFT_BACKTRACE=enable=yes,sanitize=yes,threads=all,images=all,interactive=no,swift-backtrace=./swift-backtrace-static
 
