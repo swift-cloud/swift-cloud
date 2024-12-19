0592891f6a3ca0184103628623cc0c21bba84130
Andrew Barba <barba@hey.com>
2024-12-18 19:21:10 -0500

    Remove unused pulumi plugins

--- Sources/Cloud/Pulumi/PulumiPlugins.swift
+++ Sources/Cloud/Pulumi/PulumiPlugins.swift
@@ -25,64 +25,10 @@ extension Pulumi.Plugin {
         repo: "pulumi/pulumi-awsx"
     )
 
-    // https://github.com/pulumi/pulumi-aws-native
-    public static let awsNative = Pulumi.Plugin(
-        name: "aws-native",
-        version: "1.17.0",
-        repo: "pulumi/pulumi-aws-native"
-    )
-
-    public static let azure = Pulumi.Plugin(
-        name: "azure",
-        version: "5.86.0",
-        repo: "pulumi/pulumi-azure"
-    )
-
-    public static let cloudflare = Pulumi.Plugin(
-        name: "cloudflare",
-        version: "5.35.1",
-        repo: "pulumi/pulumi-cloudflare"
-    )
-
-    public static let digitalocean = Pulumi.Plugin(
-        name: "digitalocean",
-        version: "4.31.0",
-        repo: "pulumi/pulumi-digitalocean"
-    )
-
-    public static let fastly = Pulumi.Plugin(
-        name: "fastly",
-        version: "8.9.0",
-        repo: "pulumi/pulumi-fastly"
-    )
-
-    public static let gcp = Pulumi.Plugin(
-        name: "gcp",
-        version: "7.34.0",
-        repo: "pulumi/pulumi-gcp"
-    )
-
-    public static let heroku = Pulumi.Plugin(
-        name: "heroku",
-        version: "1.0.3",
-        repo: "pulumiverse/pulumi-heroku"
-    )
-
-    public static let planetscale = Pulumi.Plugin(
-        name: "planetscale",
-        version: "0.0.7",
-        repo: "sst/pulumi-planetscale"
-    )
-
+    // https://github.com/pulumi/pulumi-random
     public static let random = Pulumi.Plugin(
         name: "random",
         version: "4.16.8",
         repo: "pulumi/pulumi-random"
-    )
-
-    public static let vercel = Pulumi.Plugin(
-        name: "vercel",
-        version: "1.11.0",
-        repo: "pulumiverse/pulumi-vercel"
     )
 }
