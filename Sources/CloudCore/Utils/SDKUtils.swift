0caf81c8891868bf6b7f10a8122d8935dcf9bb66
Andrew Barba <barba@hey.com>
2025-01-08 15:09:58 -0500

    use an enum for sdk resources

--- Sources/CloudCore/Utils/SDKUtils.swift
+++ Sources/CloudCore/Utils/SDKUtils.swift
@@ -1,6 +1,6 @@
 func writeSDKResources(_ links: [LinkProperties]) throws {
-    try? Files.removeDirectory(atPath: Context.cloudKitResourcesDirectory)
-    try Files.createDirectory(atPath: Context.cloudKitResourcesDirectory)
+    try? Files.removeDirectory(atPath: Context.cloudSDKResourcesDirectory)
+    try Files.createDirectory(atPath: Context.cloudSDKResourcesDirectory)
     for link in links {
         try writeSDKResource(link)
     }
@@ -8,24 +8,25 @@ func writeSDKResources(_ links: [LinkProperties]) throws {
 
 private func writeSDKResource(_ link: LinkProperties) throws {
     let linkName = tokenize(link.name).split(separator: "-").map { $0.capitalized }.joined()
-    let filename = "\(Context.cloudKitResourcesDirectory)/\(linkName).swift"
-
+    let filename = "\(Context.cloudSDKResourcesDirectory)/\(linkName).swift"
     let fileTemplate =
         """
         extension Cloud.Resource {
-            public struct \(linkName) {
+            public enum \(linkName) {
                 {{props}}
             }
         }
         """
-
     let fileTemplateProperties = link.properties.map { key, _ in
         """
         public static let \(key): String = env("\(link.environmentKey(key))")
         """
     }.joined(separator: "\n\t\t")
-
-    let fileContents = fileTemplate.replacingOccurrences(of: "{{props}}", with: fileTemplateProperties)
-
-    try Files.createFile(atPath: filename, contents: fileContents)
+    try Files.createFile(
+        atPath: filename,
+        contents: fileTemplate.replacingOccurrences(
+            of: "{{props}}",
+            with: fileTemplateProperties
+        )
+    )
 }
