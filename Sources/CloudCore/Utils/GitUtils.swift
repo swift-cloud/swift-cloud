584ec760012d26b52b655133f4430d7f494c11ed
Andrew Barba <barba@hey.com>
2025-01-08 14:04:13 -0500

    Add git current branch function

--- Sources/CloudCore/Utils/GitUtils.swift
+++ Sources/CloudCore/Utils/GitUtils.swift
@@ -1,18 +1,33 @@
-func updateGitignore() throws {
-    let gitignorePath = ".gitignore"
-    let entryToFind = ".cloud"
-    let entryToAdd = ".cloud/"
-
-    guard Files.fileExists(atPath: gitignorePath) else {
-        return
+public enum Git {
+    public enum GitError: Error {
+        case invalidBranch
     }
 
-    let content = try String(contentsOfFile: gitignorePath, encoding: .utf8)
-    guard !content.contains(entryToFind) else {
-        return
+    public static func currentBranch() async throws -> String {
+        let (stdout, _) = try await shellOut(to: "git", arguments: ["branch", "--show-current"])
+        let branch = stdout.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !branch.isEmpty else {
+            throw GitError.invalidBranch
+        }
+        return branch
     }
 
-    let trimmedContent = content.trimmingSuffix { $0.isWhitespace }
-    let newContent = "\(trimmedContent)\n\n# swift cloud\n\(entryToAdd)\n"
-    try newContent.write(toFile: gitignorePath, atomically: true, encoding: .utf8)
+    public static func updateGitignore() throws {
+        let gitignorePath = ".gitignore"
+        let entryToFind = ".cloud"
+        let entryToAdd = ".cloud/"
+
+        guard Files.fileExists(atPath: gitignorePath) else {
+            return
+        }
+
+        let content = try String(contentsOfFile: gitignorePath, encoding: .utf8)
+        guard !content.contains(entryToFind) else {
+            return
+        }
+
+        let trimmedContent = content.trimmingSuffix { $0.isWhitespace }
+        let newContent = "\(trimmedContent)\n\n# swift cloud\n\(entryToAdd)\n"
+        try newContent.write(toFile: gitignorePath, atomically: true, encoding: .utf8)
+    }
 }
