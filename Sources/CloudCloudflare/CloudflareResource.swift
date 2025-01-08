2c4a1c21719b18fee3c4d9efdc9a4f41d77fea12
Andrew Barba <barba@hey.com>
2025-01-07 19:44:23 -0500

    Remove dup protocol ids

--- Sources/CloudCloudflare/CloudflareResource.swift
+++ Sources/CloudCloudflare/CloudflareResource.swift
@@ -3,9 +3,3 @@ import CloudCore
 public protocol CloudflareResourceProvider: ResourceProvider {}
 
 extension Resource: CloudflareResourceProvider {}
-
-extension CloudflareResourceProvider {
-    public var id: Output<String> {
-        output.keyPath("id")
-    }
-}
--- Sources/CloudFastly/FastlyResource.swift
+++ Sources/CloudFastly/FastlyResource.swift
@@ -3,9 +3,3 @@ import CloudCore
 public protocol FastlyResourceProvider: ResourceProvider {}
 
 extension Resource: FastlyResourceProvider {}
-
-extension FastlyResourceProvider {
-    public var id: Output<String> {
-        output.keyPath("id")
-    }
-}
