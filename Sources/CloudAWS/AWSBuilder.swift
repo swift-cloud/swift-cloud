d78a419fed5d58bb8537e6c60c5e9e83d4859bf6
Andrew Barba <barba@hey.com>
2025-04-27 14:00:01 -0400

    copy output directory

--- Sources/CloudAWS/AWSBuilder.swift
+++ Sources/CloudAWS/AWSBuilder.swift
@@ -41,6 +41,15 @@ extension Builder {
             zipArguments.append("Public")
         }
 
+        // Copy Output directory if it exists
+        let outputDirectory = "\(Context.projectDirectory)/Output"
+        if Files.fileExists(atPath: outputDirectory) {
+            let lambdaOutputDirectory = "\(lambdaDirectory)/Output"
+            try Files.createDirectory(atPath: lambdaOutputDirectory)
+            try Files.copyDirectory(fromPath: outputDirectory, toPath: lambdaOutputDirectory)
+            zipArguments.append("Output")
+        }
+
         // Copy Resources directory if it exists
         let resourcesDirectory = "\(Context.projectDirectory)/Resources"
         if Files.fileExists(atPath: resourcesDirectory) {
