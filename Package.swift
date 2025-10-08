6e3b5dff9028e7fbe65d9ba8a41442fc805590d6
Andrew Barba <barba@hey.com>
2025-10-08 15:38:33 -0400

    switch back to Command

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto", "1.0.0"..<"5.0.0"),
         .package(url: "https://github.com/soto-project/soto-core", from: "7.0.0"),
+        .package(url: "https://github.com/swift-cloud/Command", from: "0.14.0"),
         .package(url: "https://github.com/swift-server/async-http-client", from: "1.0.0"),
-        .package(url: "https://github.com/swiftlang/swift-subprocess", from: "0.1.0"),
         .package(url: "https://github.com/vapor/console-kit", from: "4.0.0"),
     ],
     targets: [
@@ -51,7 +51,7 @@ let package = Package(
                 .product(name: "SotoCore", package: "soto-core"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
-                .product(name: "Subprocess", package: "swift-subprocess"),
+                .product(name: "Command", package: "Command"),
                 .product(name: "ConsoleKitTerminal", package: "console-kit"),
                 .product(name: "Crypto", package: "swift-crypto"),
             ]
--- Sources/CloudCore/Utils/Shell.swift
+++ Sources/CloudCore/Utils/Shell.swift
@@ -1,20 +1,22 @@
+import Command
 import Foundation
-import Subprocess
 
 public enum ShellError: Error, Sendable {
     case terminated(errorCode: Int32, stderr: String)
 }
 
-public enum ShellEvent: Sendable {
-    case stdout(_: String)
-    case stderr(_: String)
-    
-    func string() -> String? {
+public typealias ShellEvent = CommandEvent
+
+public enum ShellExecutable {
+    case name(_ value: String)
+    case path(_ value: String)
+
+    var value: String {
         switch self {
-        case .stderr(let text):
-            return text
-        case .stdout(let text):
-            return text
+        case .name(let value):
+            return value
+        case .path(let value):
+            return value
         }
     }
 }
@@ -24,76 +26,64 @@ public typealias ShellEventHandler = @Sendable (ShellEvent) -> Void
 private actor ShellOutputBuffer {
     private(set) var stdout = ""
     private(set) var stderr = ""
-    
+
     func appendStdout(_ line: String) {
         stdout += line + "\n"
     }
-    
+
     func appendStderr(_ line: String) {
         stderr += line + "\n"
     }
 }
 
+private let shell = CommandRunner()
+
 @discardableResult
 public func shellOut(
-    to command: Executable,
+    to command: ShellExecutable,
     arguments: [String],
     workingDirectory: String? = nil,
     environment: [String: String] = Files.currentEnvironment(),
     onEvent: ShellEventHandler? = nil
 ) async throws -> (stdout: String, stderr: String) {
-    let buffer = ShellOutputBuffer()
-    
-    let result = try await run(
-        command,
-        arguments: .init(arguments),
-        environment: .inherit,
-        workingDirectory: nil
-    ) { execution, input, out, err in
-        await withThrowingTaskGroup(of: Void.self) { group in
-            group.addTask {
-                for try await line in out.lines() {
-                    await buffer.appendStdout(line)
-                    onEvent?(ShellEvent.stdout(line))
-                }
-            }
-            group.addTask {
-                for try await line in err.lines() {
-                    await buffer.appendStderr(line)
-                    onEvent?(ShellEvent.stderr(line))
-                }
-            }
-        }
-    }
-    
-    switch result.terminationStatus {
-    case .exited(let code) where code > 0:
-        throw ShellError.terminated(errorCode: code, stderr: await buffer.stderr)
-    case .unhandledException(let code):
-        throw ShellError.terminated(errorCode: code, stderr: await buffer.stderr)
-    default:
-        break
-    }
+    var std = ""
+    var stdout = ""
+    var stderr = ""
+    let stream = try shellStream(
+        to: command,
+        arguments: arguments,
+        workingDirectory: workingDirectory,
+        environment: environment
+    )
+    do {
+        for try await line in stream {
+            std += line.string() ?? ""
+            switch line {
+            case .standardOutput:
+                stdout += line.string() ?? ""
+            case .standardError:
+                stderr += line.string() ?? ""
 
-    return await (buffer.stdout, buffer.stderr)
+            }
+            onEvent?(line)
+        }
+        return (stdout, stderr)
+    } catch let CommandError.terminated(errorCode, _) {
+        throw ShellError.terminated(errorCode: errorCode, stderr: std)
+    } catch {
+        throw ShellError.terminated(errorCode: 255, stderr: std)
+    }
 }
 
 public func shellStream(
-    to command: Executable,
+    to command: ShellExecutable,
     arguments: [String],
     workingDirectory: String? = nil,
     environment: [String: String] = ProcessInfo.processInfo.environment
 ) throws -> AsyncThrowingStream<ShellEvent, any Error> {
-    return AsyncThrowingStream<ShellEvent, Error>(ShellEvent.self) { continuation in
-        Task.detached {
-            try? await shellOut(
-                to: command,
-                arguments: arguments,
-                workingDirectory: workingDirectory,
-                environment: environment
-            ) {
-                continuation.yield($0)
-            }
-        }
-    }
+    return shell.run(
+        arguments: [command.value] + arguments,
+        environment: environment,
+        workingDirectory: try workingDirectory.map { try .init(validating: $0) }
+    )
 }
