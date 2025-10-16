bc7d8593fc8b2424876700ddb84ed00f98b46a78
Andrew Barba <barba@hey.com>
2025-10-16 13:55:56 -0400

    fix: remove cert lookup logic

--- Sources/CloudAWS/Resources/TLSCertificate.swift
+++ Sources/CloudAWS/Resources/TLSCertificate.swift
@@ -1,8 +1,10 @@
 extension AWS {
     public struct TLSCertificate: AWSResourceProvider {
         public let resource: Resource
-        public let arn: Output<String>
-        private let existingCertLookup: Variable<GetCertificate?>?
+
+        public var arn: Output<String> {
+            resource.output.keyPath("arn")
+        }
 
         public var domainValidationOptions:
             Output<[(resourceRecordName: String, resourceRecordType: String, resourceRecordValue: String)]>
@@ -17,85 +19,17 @@ extension AWS {
         public init(
             hostname: any Input<String>,
             keyAlgorithm: KeyAlgorithm = .ecdsa,
-            reuseExisting: Bool = true,
             options: Resource.Options? = nil,
             context: Context = .current
         ) {
-            if reuseExisting {
-                // Look for existing certificate
-                self.existingCertLookup = Variable<GetCertificate?>.invoke(
-                    name: "\(hostname)-cert-lookup",
-                    function: "aws:acm:getCertificate",
-                    arguments: [
-                        "domain": hostname,
-                        "mostRecent": true,
-                        "statuses": ["ISSUED"]
-                    ]
-                )
-                
-                // Create the certificate resource
-                // Pulumi will handle conflicts by showing import suggestions
-                resource = Resource(
-                    name: "\(hostname)-cert",
-                    type: "aws:acm:Certificate",
-                    properties: [
-                        "domainName": hostname,
-                        "keyAlgorithm": keyAlgorithm.rawValue,
-                        "validationMethod": "DNS",
-                    ],
-                    options: options,
-                    context: context
-                )
-                
-                self.arn = resource.output.keyPath("arn")
-            } else {
-                self.existingCertLookup = nil
-                
-                // Always create new certificate
-                resource = Resource(
-                    name: "\(hostname)-cert",
-                    type: "aws:acm:Certificate",
-                    properties: [
-                        "domainName": hostname,
-                        "keyAlgorithm": keyAlgorithm.rawValue,
-                        "validationMethod": "DNS",
-                    ],
-                    options: options,
-                    context: context
-                )
-                
-                self.arn = resource.output.keyPath("arn")
-            }
-        }
-        
-        /// Check if an existing certificate was found for this domain
-        public var hasExistingCertificate: Output<Bool> {
-            if let existingCertLookup {
-                return existingCertLookup.output.keyPath("arn")
-            }
-            return Output(prefix: "false", root: "", path: [])
-        }
-        
-        /// Get the existing certificate ARN if available  
-        /// This can be used to conditionally skip DNS validation if certificate already exists
-        public var existingCertificateArn: Output<String?> {
-            if let existingCertLookup {
-                return existingCertLookup.output.keyPath("arn")
-            }
-            return Output(prefix: "null", root: "", path: [])
-        }
-        
-        /// Returns existing certificate if found, otherwise creates new one
-        public static func getOrCreate(
-            hostname: any Input<String>,
-            keyAlgorithm: KeyAlgorithm = .ecdsa,
-            options: Resource.Options? = nil,
-            context: Context = .current
-        ) -> TLSCertificate {
-            return TLSCertificate(
-                hostname: hostname,
-                keyAlgorithm: keyAlgorithm,
-                reuseExisting: true,
+            resource = Resource(
+                name: "\(hostname)-cert",
+                type: "aws:acm:Certificate",
+                properties: [
+                    "domainName": hostname,
+                    "keyAlgorithm": keyAlgorithm.rawValue,
+                    "validationMethod": "DNS",
+                ],
                 options: options,
                 context: context
             )
